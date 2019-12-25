42f8b1c163fe25adb2955b64adb4ff64da9d01a5
s523005941 <523005941@qq.com>
2019-12-25 11:42:31 +0800

    删除了注释

--- src/main/java/com/newer/util/SqlSessionUtil.java
+++ src/main/java/com/newer/util/SqlSessionUtil.java
@@ -18,7 +18,6 @@ public class SqlSessionUtil {
     static {
         try {
             //读取主配置文件
-            //黄诚是条狗999
             Reader reader= Resources.getResourceAsReader("mybatis-config.xml");
             //加载并按照配置文件的描述生成sqlSessionFactory
             sqlSessionFactory=new SqlSessionFactoryBuilder().build(reader);
