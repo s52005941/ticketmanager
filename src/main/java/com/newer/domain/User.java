f656ed790b96c0cdb39681325807afa98895e0ec
s523005941 <523005941@qq.com>
2019-12-26 14:24:18 +0800

    添加了图片验证码 以及注册页面和个人资料页面

--- src/main/java/com/newer/domain/User.java
+++ src/main/java/com/newer/domain/User.java
@@ -1,8 +1,127 @@
 package com.newer.domain;
 
+import java.sql.Date;
+
 /**
  * 用户实体类
  */
 public class User {
+    private int uid; //用户id
+    private String uname; //用户账号
+    private String password; //用户密码
+    private Date birthday;  //出生日期
+    private String email;  //邮箱
+    private String area;   //所在区域
+    private String address;  //街道住址
+    private String coding;   //邮政编码
+    private String identity; //身份证号
+    private String phone;  //手机号码
+    private String sex;   //性别
+    private String status; //激活状态  N代表未激活 Y代表激活
+    private String code;  //激活码 （唯一）
 
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUname() {
+        return uname;
+    }
+
+    public void setUname(String uname) {
+        this.uname = uname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCoding() {
+        return coding;
+    }
+
+    public void setCoding(String coding) {
+        this.coding = coding;
+    }
+
+    public String getIdentity() {
+        return identity;
+    }
+
+    public void setIdentity(String identity) {
+        this.identity = identity;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
 }
